4ddf2da2100ef210c06930374795f92e6f5ead5a
CJAndrew84 <50639116+CJAndrew84@users.noreply.github.com>
2022-07-15 08:55:57 +0100

    Updated Structure, added IFC Road and Drainage

--- src/Program.cs
+++ src/Program.cs
@@ -3,8 +3,10 @@
 var db = new DatabaseIfc(ReleaseVersion.IFC4X3_RC4);
 var site = new IfcSite(db, "MySite");
 
+var project = new IfcProject(site, "MyProject", IfcUnitAssignment.Length.Metre);
+{
+};
 
-var facility = new IfcFacility(db, "MyFacility");
 
 var bridge = new IfcBridge(db)
 {
@@ -12,9 +14,10 @@ var bridge = new IfcBridge(db)
     Description = "Just another bridge"
 };
 
-var project = new IfcProject(site, "MyProject", IfcUnitAssignment.Length.Metre);
-{
-};
+
+var facility = new IfcFacility(site, "MyFacility");
+
+
 
 
 var foundations = new IfcFacilityPart(
@@ -38,7 +41,7 @@ footingType.RepresentationMaps.Add(
             new IfcExtrudedAreaSolid(
                 new IfcRectangleHollowProfileDef(db, name, length, width, depth),
                 new IfcAxis2Placement3D(
-                    new IfcCartesianPoint(db, 0 ,0, 0)
+                    new IfcCartesianPoint(db, 0, 0, 0)
                     ),
                 db.Factory.ZAxisNegative,
                 depth
@@ -46,6 +49,53 @@ footingType.RepresentationMaps.Add(
         )
     )
 );
+
+// IfcRoad road1 = new IfcRoad(site, "my road", null);
+
+
+
+var road = new IfcFacility(site, "My Road")
+{
+    CompositionType = IfcElementCompositionEnum.COMPLEX
+};
+
+var CarriagewayA = new IfcFacilityPart(road, "Carriageway A", new IfcFacilityPartTypeSelect(IfcRoadPartTypeEnum.ROADSEGMENT), IfcFacilityUsageEnum.LONGITUDINAL);
+var CarriagewayB = new IfcFacilityPart(road, "Carriageway B", new IfcFacilityPartTypeSelect(IfcRoadPartTypeEnum.ROADSEGMENT), IfcFacilityUsageEnum.LONGITUDINAL);
+
+
+
+var pavement = new IfcFacilityPart(
+    CarriagewayA,
+    "Carriageway",
+    new IfcFacilityPartTypeSelect(IfcFacilityPartCommonTypeEnum.BELOWGROUND),
+    IfcFacilityUsageEnum.NOTDEFINED
+    );
+
+IfcPavementType pavementType = new IfcPavementType(db, "Pavement", IfcPavementTypeEnum.FLEXIBLE);
+
+pavementType.RepresentationMaps.Add(
+
+    new IfcRepresentationMap(
+        pavement.Database.Factory.XYPlanePlacement,
+        new IfcShapeRepresentation(
+            new IfcExtrudedAreaSolid(
+                new IfcRectangleHollowProfileDef(pavement.Database, "Surface Course", 200, 3.65, 0.4),
+                new IfcAxis2Placement3D(
+                    new IfcCartesianPoint(db, 0, 0, 0)
+                    ),
+                db.Factory.ZAxisNegative,
+                0.4
+            )
+            )
+        )
+    );
+
+var drainage = new IfcDistributionSystem(
+    road,
+    "Drainage",
+    IfcDistributionSystemEnum.DRAINAGE
+    );
+
 
 db.WriteFile("IFC4X3RC4_testBridge.ifc");
 
